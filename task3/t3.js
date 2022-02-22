4f5bb625f9caf9736ecd9aef40c1dc5f9efce18c
Aki Rytkönen <the.rytkonen@gmail.com>
2022-02-22 14:05:32 +0200

    little clean it, console log changes

--- task3/t3.js
+++ task3/t3.js
@@ -6,16 +6,14 @@ function solution(A) {
   while(i<A.length){
     marker = A[i]
 
-    let plus = 0
-    let minus = 0
+    let reduction = 0
     let count = 0
     for(let a = 0 ; 0 < A[a]; a++){
       if(A[a] == marker)count++
     }
 
-    minus=count
-    plus=Math.abs(marker-count)
-    result += Math.min(plus, minus)
+    reduction=Math.abs(marker-count)
+    result += Math.min(reduction, count)
 
     i += count
   }
@@ -23,7 +21,12 @@ function solution(A) {
   return result
 }
 
-console.log(solution([1,1,3,4,4,4]), " tulos pitäisi olla 3")
-console.log(solution([1,2,2,2,5,5,5,8]), " tulos pitäisi olla 4")
-console.log(solution([1,1,1,1,3,3,4,4,4,4,4]), " tulos pitäisi olla 5")
-console.log(solution([10,10,10]), " tulos pitäisi olla 3")
+const array1 = [1,1,3,4,4,4]
+const array2 = [1,2,2,2,5,5,5,8]
+const array3 = [1,1,1,1,3,3,4,4,4,4,4]
+const array4 = [10,10,10]
+
+console.log("with array [" + array1 + "] we should make it with 3 steps and solution returns " + solution(array1) + " steps")
+console.log("with array [" + array2 + "] we should make it with 4 steps and solution returns " + solution(array2) + " steps")
+console.log("with array [" + array3 + "] we should make it with 5 steps and solution returns " + solution(array3) + " steps")
+console.log("with array [" + array4 + "] we should make it with 3 steps and solution returns " + solution(array4) + " steps")
